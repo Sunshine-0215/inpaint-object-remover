ca196ee28e3dbc2c684f8f04e850b352e18421fe
sunshine <sunshine021599@gmail.com>
2019-11-26 18:53:14 +0800

    添加全部注释

--- inpainter/__main__.py
+++ inpainter/__main__.py
@@ -38,7 +38,7 @@ def parse_args():
         '--plot-progress',
         help='plot each generated image',
         action='store_true',
-        default=True
+        default=False
     )
     parser.add_argument(
         '-i',
